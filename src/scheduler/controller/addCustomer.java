1e0b601fd354b4bb4b313fad4476348592a0bb13
Chris <cshor68@wgu.edu>
2023-03-26 08:46:33 -0600

    Added addCustomer view and controller

---
+++ src/scheduler/controller/addCustomer.java
@@ -0,0 +1,11 @@
+package scheduler.controller;
+
+import javafx.event.ActionEvent;
+
+public class addCustomer {
+    public void saveClick(ActionEvent actionEvent) {
+    }
+
+    public void cancelClick(ActionEvent actionEvent) {
+    }
+}
