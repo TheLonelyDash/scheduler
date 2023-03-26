14924a6bfe6b73139878dba836c94219baf44793
Chris <cshor68@wgu.edu>
2023-03-26 08:32:25 -0600

    Added Main Menu controller and view

---
+++ src/scheduler/controller/mainMenuController.java
@@ -0,0 +1,21 @@
+package scheduler.controller;
+
+import javafx.event.ActionEvent;
+
+public class mainMenuController {
+
+
+    public void mainMenuAppointmentsClick(ActionEvent actionEvent) {
+    }
+
+    public void mainMenuCustomersClick(ActionEvent actionEvent) {
+    }
+
+    public void mainMenuReportsClick(ActionEvent actionEvent) {
+    }
+
+    public void mainMenuExitClick(ActionEvent actionEvent) {
+    }
+
+
+}
