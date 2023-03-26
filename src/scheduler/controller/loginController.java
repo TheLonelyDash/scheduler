c136e887827dc384c09959a8cdb18c4abb5ca4ee
Chris <cshor68@wgu.edu>
2023-03-26 08:28:53 -0600

    test commit

---
+++ src/scheduler/controller/loginController.java
@@ -0,0 +1,20 @@
+package scheduler.controller;
+
+import javafx.event.ActionEvent;
+
+public class loginController {
+
+
+
+    public void loginButtonClick(ActionEvent actionEvent) {
+    }
+
+
+
+
+    public void exitButtonClick(ActionEvent actionEvent) {
+    }
+
+
+
+}
