fb2de907b97daed53024f8dfe310224eb80fecc5
Chris <cshor68@wgu.edu>
2023-03-29 14:48:43 -0600

    Added alerts.java

---
+++ src/scheduler/models/alerts.java
@@ -0,0 +1,18 @@
+package scheduler.models;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import java.util.Optional;
+
+public class alerts {
+
+    public static boolean alert(String x,String y,String z){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(x);
+        alert.setHeaderText(y);
+        alert.setContentText(z);
+        Optional<ButtonType> result = alert.showAndWait();
+        return (result.get() == ButtonType.OK);
+    }
+
+}
