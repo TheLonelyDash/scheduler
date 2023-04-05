c4ad5d17e1e203a03079ec42da351d669739f8d2
Chris <cshor68@wgu.edu>
2023-04-04 18:16:50 -0600

    created successful and unsuccessful login methods which create a .txt file

--- src/scheduler/controller/loginController.java
+++ src/scheduler/controller/loginController.java
@@ -5,9 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
-//import java.awt.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,14 +19,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import scheduler.model.alerts;
-import scheduler.utilities.DBQuery;
 import scheduler.utilities.userSearch;
 
 import java.lang.System;
 import java.util.*;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -35,6 +32,10 @@ import java.time.ZoneId;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.Optional;
+
+interface LogActivity {
+    public String getFileName();
+}
 
 
 public class loginController implements Initializable {
@@ -52,6 +53,7 @@ public class loginController implements Initializable {
 
     @FXML
     public void loginButtonClick(ActionEvent actionEvent) throws IOException, SQLException {
+        createFile();
         if (usernameTextField.getText().isEmpty()) {
             if (Locale.getDefault().getLanguage() == "en") {
                 alerts.alert("Username", "No username was provided.", "Please enter a valid username!");
@@ -70,6 +72,7 @@ public class loginController implements Initializable {
             boolean condition = userSearch.checkNameAndPassword(usernameTextField.getText(), passwordTextField.getText());
 
             if (condition == true) {
+                successfulLogin();
                 Parent root = FXMLLoader.load(getClass().getResource("/scheduler/view/mainMenu.fxml"));
                 Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
                 Scene scene = new Scene(root);
@@ -82,17 +85,16 @@ public class loginController implements Initializable {
                 stage.show();
             }
             else {
+                unsuccessfulLogin();
                 if (Locale.getDefault().getLanguage() == "en"){
                     alerts.alert("Login Credentials", "Incorrect Username or Password.", "Please enter a valid username and password.");
                 }
                 else {
                     alerts.alert("Identifiants de Connexion", "Identifiant ou mot de passe incorrect.", "Veuillez saisir un nom d'utilisateur et un mot de passe valides.");
                 }
-
             }
         }
     }
-
 
 
     @FXML
@@ -109,6 +111,71 @@ public class loginController implements Initializable {
 
 
 
+
+
+
+
+
+
+
+
+
+
+    LogActivity logActivity = () -> {
+        return "login_activity.txt";
+    };
+
+
+    private void createFile(){
+        try {
+            File newFile = new File(logActivity.getFileName());
+            if (newFile.createNewFile()) {
+                System.out.println("You created a new file at " + newFile.getName());
+            }
+            else {
+                System.out.println("Yo! You already created this file! It's location is "+ newFile.getPath());
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void successfulLogin(){
+        try {
+            FileWriter fileWriter = new FileWriter(logActivity.getFileName(), true);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            Date date = new Date(System.currentTimeMillis());
+            fileWriter.write("The Login was SUCCESSFUL. \n Username: " + usernameTextField.getText() + "\nPassword: " + passwordTextField.getText() + "\nTimestamp: " + simpleDateFormat.format(date) + "\n\n");
+            fileWriter.close();
+            System.out.println("Successful Login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void unsuccessfulLogin() {
+        try {
+            FileWriter fileWriter = new FileWriter(logActivity.getFileName(), true);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            Date date = new Date(System.currentTimeMillis());
+            fileWriter.write("The Login was UNSUCCESSFUL. \n Username: " + usernameTextField.getText() + "\nPassword: " + passwordTextField.getText() + "\nTimestamp: " + simpleDateFormat.format(date) + "\n\n");
+            fileWriter.close();
+            System.out.println("Unuccessful Login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -121,5 +188,5 @@ public class loginController implements Initializable {
         exitButton.setText(rb.getString("exit"));
         locationTextField.setText(rb.getString("country"));
     }
-
 }
+
