e0e7f5e29bfc8c04d84d3750d4ccf1effe96a700
Chris <cshor68@wgu.edu>
2023-04-14 07:41:11 -0600

    Reports total month and appointment complete.

--- src/scheduler/controller/reportsController.java
+++ src/scheduler/controller/reportsController.java
@@ -1,5 +1,6 @@
 package scheduler.controller;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,6 +17,9 @@ import scheduler.utilities.appointmentSearch;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -35,9 +39,92 @@ public class reportsController implements Initializable {
     @FXML TableColumn<?, ?> startCol;
     @FXML TableColumn<?, ?> endCol;
     @FXML TableColumn<?, ?> custIDCol;
-    @FXML TableColumn<?, ?> locationCol;
-    @FXML TableColumn<?, ?> contactCol;
-    @FXML TableColumn<?, ?> user_IDCol;
+
+    @FXML TextField planningSessionsField;
+    @FXML TextField debriefingField;
+    @FXML TextField otherField;
+
+    @FXML TextField january;
+    @FXML TextField february;
+    @FXML TextField march;
+    @FXML TextField april;
+    @FXML TextField may;
+    @FXML TextField june;
+    @FXML TextField july;
+    @FXML TextField august;
+    @FXML TextField september;
+    @FXML TextField october;
+    @FXML TextField november;
+    @FXML TextField december;
+
+
+    public void totalAppointments() {
+
+        ObservableList<String> planningSessions = FXCollections.observableArrayList();
+        ObservableList<String> debriefings = FXCollections.observableArrayList();
+        ObservableList<String> other = FXCollections.observableArrayList();
+
+        ObservableList<Integer> jan = FXCollections.observableArrayList();
+        ObservableList<Integer> feb = FXCollections.observableArrayList();
+        ObservableList<Integer> mar = FXCollections.observableArrayList();
+        ObservableList<Integer> apr = FXCollections.observableArrayList();
+        ObservableList<Integer> mayMonth = FXCollections.observableArrayList();
+        ObservableList<Integer> jun = FXCollections.observableArrayList();
+        ObservableList<Integer> jul = FXCollections.observableArrayList();
+        ObservableList<Integer> aug = FXCollections.observableArrayList();
+        ObservableList<Integer> sep = FXCollections.observableArrayList();
+        ObservableList<Integer> oct = FXCollections.observableArrayList();
+        ObservableList<Integer> nov = FXCollections.observableArrayList();
+        ObservableList<Integer> dec = FXCollections.observableArrayList();
+
+        try {
+            ObservableList<appointment> appointments = appointmentSearch.getAllAppointments();
+
+            if (appointments != null) {
+                for (appointment appointment : appointments) {
+                    String type = appointment.getType();
+                    LocalDate date = appointment.getStartDate();
+
+                    if (type.equals("Planning Session")) {planningSessions.add(type);}
+                    if (type.equals("De-Briefing")) {debriefings.add(type);}
+                    if (type.equals("other")) {other.add(type);}
+
+                    if (date.getMonth().equals(Month.of(1))) {jan.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(2))) {feb.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(3))) {mar.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(4))) {apr.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(5))) {mayMonth.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(6))) {jun.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(7))) {jul.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(8))) {aug.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(9))) {sep.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(10))) {oct.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(11))) {nov.add(date.getMonthValue());}
+                    if (date.getMonth().equals(Month.of(12))) {dec.add(date.getMonthValue());}
+                }
+            }
+            planningSessionsField.setText(String.valueOf(planningSessions.size()));
+            debriefingField.setText(String.valueOf(debriefings.size()));
+            otherField.setText(String.valueOf(other.size()));
+
+            january.setText(String.valueOf(jan.size()));
+            february.setText(String.valueOf(feb.size()));
+            march.setText(String.valueOf(mar.size()));
+            april.setText(String.valueOf(apr.size()));
+            may.setText(String.valueOf(mayMonth.size()));
+            june.setText(String.valueOf(jun.size()));
+            july.setText(String.valueOf(jun.size()));
+            august.setText(String.valueOf(aug.size()));
+            september.setText(String.valueOf(sep.size()));
+            october.setText(String.valueOf(oct.size()));
+            november.setText(String.valueOf(nov.size()));
+            december.setText(String.valueOf(dec.size()));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     public void reportBackButtonClick(ActionEvent actionEvent) throws IOException {
@@ -54,6 +141,7 @@ public class reportsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        totalAppointments();
 
         try {
             appointments = appointmentSearch.getAllAppointments();
